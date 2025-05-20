41ece84ebecde99b725923283b887e864012848c
Alessio Buccino <alejoe9187@gmail.com>
2025-05-20 15:11:31 +0200

    Cleanup unused apps for space

--- pipeline/test_local.sh
+++ pipeline/test_local.sh
@@ -5,5 +5,5 @@ fi
 
 
 DATA_PATH="$PWD/../sample_dataset/nwb/" RESULTS_PATH="$PWD/../sample_dataset/nwb_results/" \
-    nextflow -C nextflow_local.config -log $RESULTS_PATH/nextflow/nextflow.log run main_local.nf \
+    nextflow -C nextflow_local.config -log $RESULTS_PATH/nextflow/nextflow.log run main_multi_backend.nf \
     --sorter spykingcircus2 --runmode fast --job_dispatch_args "--input nwb" --preprocessing_args "--motion skip" $RESUME_FLAG
