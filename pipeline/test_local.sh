10805854165d17668d8c1b8d7333169144a2dddf
Alessio Buccino <alejoe9187@gmail.com>
2025-06-03 17:10:56 +0200

    update postprocessing

--- pipeline/test_local.sh
+++
@@ -1,9 +0,0 @@
-RESUME_FLAG=""
-if [ "$1" == "-resume" ]; then
-    RESUME_FLAG="-resume"
-fi
-
-
-DATA_PATH="$PWD/../sample_dataset/nwb/" RESULTS_PATH="$PWD/../sample_dataset/nwb_results/" \
-    nextflow -C nextflow_local.config -log $RESULTS_PATH/nextflow/nextflow.log run main_multi_backend.nf \
-    --sorter spykingcircus2 --runmode fast --job_dispatch_args "--input nwb" --preprocessing_args "--motion skip" $RESUME_FLAG
